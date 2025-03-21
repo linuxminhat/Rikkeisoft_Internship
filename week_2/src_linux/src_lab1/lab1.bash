2ab8730b168d2448df22cc7131fe9af7d077ced6
Đặng Nhật Minh <163006511+linuxminhat@users.noreply.github.com>
2025-03-21 17:13:39 +0700

    Update lab1.bash

--- week_2/src_linux/src_lab1/lab1.bash
+++ week_2/src_linux/src_lab1/lab1.bash
@@ -1,12 +1,8 @@
 #!/bin/bash
-
-# Đường dẫn tới thư mục log
 LOG_DIR="/var/log"
 
-# Di chuyển vào thư mục log
 cd "$LOG_DIR" || exit
 
-# Nén các file có đuôi .1 và .old
 for file in *.1 *.old; do
     if [ -f "$file" ]; then
         gzip "$file"
